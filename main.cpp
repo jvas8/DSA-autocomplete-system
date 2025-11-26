1a03a245b43e4f3a27483a2caa80c60f7e3a575e
Jeimy Vasquez <2024161429@ub.edu.bz>
2025-11-26 14:04:50 +0000

    Added dictionary of words

--- main.cpp
+++ main.cpp
@@ -104,6 +104,20 @@ public:
 
 int main() {
      Trie t;
+    vector<string> dictionary = {
+    "the","of","and","to","a","in","for","is","on","that",
+    "by","this","with","i","you","it","not","or","be",
+    "are","from","at","as","your","all","have","new","more",
+    "an","was","we","will","home","can","us","about","if",
+    "page","my","has","search","free","but","our","one","other",
+    "do","no","information","time"
+};
+    for (auto &w : dictionary)
+        t.insert(w);
+
+
+    cout << "Loaded " << dictionary.size() << " words.\n";
+
 
     t.insert("cat");
     t.insert("car");
